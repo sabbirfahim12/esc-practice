a83f861682c9e70df4fdd51490c4a886da268067
sabbir fahim <iamsabbir126@gamil.com>
2020-03-03 00:33:53 +0600

    commited multiline tricks

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-var speech="i am a student i am reading class alim."
+const firstName="sabbir";
+const lastName="fahim";
+
+const add=firstName+" "+ lastName;
+const add2=`${firstName} ${20+78} is a good boy`;
+console.log(add2);
+const multiline=`i am  fahim i am reading class alim
+hi there i am sabbir fahim i am reading class alim
+`
+console.log(multiline)
