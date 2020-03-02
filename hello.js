12eb9af019302498e854773a8ea58108b75ff130
sabbir fahim <iamsabbir126@gamil.com>
2020-03-03 01:59:03 +0600

    some added

---
+++ hello.js
@@ -0,0 +1,29 @@
+/*function doubleit(num){
+return num*2;
+
+}
+const result =doubleit(5);
+console.log(result);*/
+/*
+const doubleit=function myfan(num1){
+return num1*3;
+}
+const result2=doubleit(5);
+console.log(result2)*/
+
+/*const doubleit=num3=>num3*50;
+const result2=doubleit(5);
+console.log(result2)*/
+
+/*const add=(x,y)=>x+y;
+const result2=add(5,45);
+console.log(result2)*/
+
+const bishal=(x,y)=>{
+const add=x+y;
+const sub=x-y;
+const total=add*sub;
+return total;
+}
+const fulloutput=bishal(10,5);
+console.log(fulloutput);
