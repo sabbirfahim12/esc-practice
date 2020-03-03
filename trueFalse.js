dcb317b08d4651810d2186bb81772af075a4abba
sabbir fahim <iamsabbir126@gamil.com>
2020-03-03 23:44:55 +0600

    trueFalse added

---
+++ trueFalse.js
@@ -0,0 +1,17 @@
+//falsey
+//1.""
+//2.0
+//3.null
+//4.undefined
+//5.Nan
+//6.false
+
+//truthy
+//1.baki sob
+const name="fahim";
+if(name.length){
+console.log("conditin is right");
+
+}else{
+    console.log("conditon is false")
+}
