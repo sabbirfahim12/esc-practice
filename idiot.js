39479d22004e523c2b0471e91009e6703586c4f9
sabbir fahim <iamsabbir126@gamil.com>
2020-03-03 20:08:24 +0600

    some added

---
+++ idiot.js
@@ -0,0 +1,16 @@
+/*const ages=[12,34,43,13];
+const ages2=[23,34,13];
+const ages3=[12,23,43];
+//const allages=ages.concat(ages2).concat(1000).concat(ages3);
+const allages=[...ages,...ages2,5,...ages3];
+console.log(allages);
+const minister=345;
+const buisness=3456;
+const sochib=1111;
+//const miximum=Math.max(minister,buisness,sochib);
+console.log(miximum);*/
+
+
+const miximum2=[234,3435,46,121];
+const miximum3=Math.max(...miximum2);
+console.log(miximum3);
