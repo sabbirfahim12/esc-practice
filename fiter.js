75e6925a9d35698d4b2ed1cacea93b1814787f5e
sabbir fahim <iamsabbir126@gamil.com>
2020-03-04 19:43:11 +0600

    some added

--- fiter.js
+++ fiter.js
@@ -2,7 +2,9 @@
 //find mane pasor boro aktare output dekaibo
 const numbers=[3,5,2,6,9];
 const bigger=numbers.filter(element=>element>5);
-console.log(bigger)
+console.log(bigger);
+const map=numbers.map(numbers=>numbers*2);
+console.log(map)
 
 
 
